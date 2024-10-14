b0b1fefa23b8a08219bbe6bddcc7111f53c0a959
Chinseok Lee <me@pyhub.kr>
2024-10-14 14:01:59 +0900

    updated ui

--- example1/main.py
+++ example1/main.py
@@ -1,13 +1,65 @@
 import flet as ft
+from core import BaseRootView, MainPage
+
+
+class WebtoonView(BaseRootView):
+    path = "/webtoons/"
+    nav_icon = ft.icons.HOME
+    nav_label = "웹툰"
+    title = "알아서 딱!"
+
+    def make_appbar(self) -> ft.AppBar:
+        return ft.AppBar(
+            leading=ft.GestureDetector(
+                ft.Icon(ft.icons.HOME), on_tap=lambda e: print("clicked leading")
+            ),
+            title=ft.Text(self.title),
+            actions=[
+                ft.IconButton(icon=ft.icons.SEARCH),
+            ],
+        )
+
+    def make_layout(self) -> ft.Control:
+        return ft.Column(
+            [
+                ft.Text("알아서 딱! 페이지"),
+            ]
+        )
+
+
+class WebtoonRecommendView(BaseRootView):
+    path = "/recommend/"
+    nav_icon = ft.icons.ADD
+    nav_label = "추천완결"
+    title = "추천완결"
+
+    def make_appbar(self) -> ft.AppBar:
+        return ft.AppBar(
+            title=ft.Text(self.nav_label),
+            actions=[
+                ft.IconButton(icon=ft.icons.SEARCH),
+            ],
+        )
+
+    def make_layout(self) -> ft.Control:
+        return ft.Column(
+            [
+                ft.Text("추천완결 페이지"),
+            ]
+        )
 
 
 def main(page: ft.Page) -> None:
-    page.add(
-        ft.Container(
-            content=ft.Text("hello flet #5", size=48),
-            alignment=ft.alignment.center,
-            expand=True
-        )
+    page.adaptive = True  # 적응형 옵션 활성화
+    page.padding = 0
+    page.window.min_width = 480
+
+    MainPage(
+        root_view_classes=[
+            WebtoonView,
+            WebtoonRecommendView,
+        ],
+        page=page,
     )
 
 
