25d8e12adf0a5481066890f81725db1c2991bf12
Chinseok Lee <me@pyhub.kr>
2024-10-14 13:50:31 +0900

    updated flutter 3.24.0

--- example1/main.py
+++ example1/main.py
@@ -2,7 +2,13 @@ import flet as ft
 
 
 def main(page: ft.Page) -> None:
-    page.add(ft.Text("hello flet #4"))
+    page.add(
+        ft.Container(
+            content=ft.Text("hello flet #5", size=48),
+            alignment=ft.alignment.center,
+            expand=True
+        )
+    )
 
 
 ft.app(target=main)
